fa365e47a5cc8de7c2e7581c92306087dc85f95a
U-Yeong Ju <uyeong21c@gmail.com>
2019-11-12 14:27:24 +0900

    setup document site directory

---
+++ docs/_next/static/_QxV9AF348ek29xm1ZzZV/pages/index.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"/EDR":function(n,t,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return e("23aj")}])},"23aj":function(n,t,e){"use strict";e.r(t);var o=e("q1tI"),u=o.createElement;t.default=function(){return u("h1",null,"Hello belt \ud83d\udc4b")}}},[["/EDR",0,1]]]);
