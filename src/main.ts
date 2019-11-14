0f01d70266b7b8d125e7535e449a114b632182fa
U-Yeong Ju <uyeong21c@gmail.com>
2019-11-14 20:24:18 +0900

    write easing change and loop logic

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import EventEmitter3 from 'eventemitter3';
+import EventEmitter from 'eventemitter3';
 
 interface BeltOption {
   delay?: number;
@@ -18,37 +18,44 @@ type BeltEasingFn = (n: number) => number;
 const root = (typeof window === 'undefined' ? global : window) as Window;
 
 function blender(easing: BeltEasingFn, reverse: boolean) {
-  return (n: number) => (reverse ? 1 - easing(n) : easing(n));
+  return (progress: number, turn: boolean) => {
+    // prettier-ignore
+    return turn ?
+      reverse ? easing(progress) : 1 - easing(progress) :
+      reverse ? 1 - easing(progress) : easing(progress);
+  };
 }
 
 class Belt {
+  private emitter: EventEmitter = new EventEmitter();
   private delay: number;
   private duration: number;
   private loop: boolean;
   private reverse: boolean;
   private round: boolean;
   private easing: BeltEasingFn;
-  private emitter: EventEmitter3;
+  private blend: (n: number, t: boolean) => number;
+  private turn: boolean = false;
   private timestamp: number = 0;
   private startTime: number = 0;
   private pastTime: number = 0;
   private rafId: number = 0;
 
   constructor(option: BeltOption = {}) {
-    this.emitter = new EventEmitter3();
     this.delay = option.delay ?? 0;
     this.duration = option.duration ?? 0;
     this.loop = option.loop ?? false;
     this.reverse = option.reverse ?? false;
     this.round = option.round ?? false;
     this.easing = option.easing ?? ((n: number) => n);
+    this.blend = blender(this.easing, this.reverse);
   }
 
   public option(): BeltOption;
-  public option(key: BeltOption): void;
+  public option(key: Partial<BeltOption>): void;
   public option<T extends keyof BeltOption>(key: T): BeltOption[T];
   public option<T extends keyof BeltOption>(key: T, value: BeltOption[T]): void;
-  public option(key?: keyof BeltOption | BeltOption, value?: BeltOption[keyof BeltOption]): any {
+  public option(key?: keyof BeltOption | Partial<BeltOption>, value?: BeltOption[keyof BeltOption]): any {
     if (key === undefined && value === undefined) {
       return {
         delay: this.delay,
@@ -79,13 +86,17 @@ class Belt {
         if (currOption[prop] !== nextOption[prop]) {
           // @ts-ignore
           this[prop] = nextOption[prop];
-          if (prop === 'reverse') {
-            this.pastTime = this.duration * (1 - this.pastTime / this.duration);
-            this.startTime = this.timestamp - this.pastTime;
-          }
           if (prop === 'duration') {
             this.pastTime = this.duration * (this.pastTime / currOption.duration);
             this.startTime = this.timestamp - this.pastTime;
+          }
+          if (prop === 'reverse') {
+            this.pastTime = this.duration * (1 - this.pastTime / this.duration);
+            this.startTime = this.timestamp - this.pastTime;
+            this.blend = blender(this.easing, this.reverse);
+          }
+          if (prop === 'easing') {
+            this.blend = blender(this.easing, this.reverse);
           }
         }
       }
@@ -103,16 +114,20 @@ class Belt {
       this.pastTime = timestamp - this.startTime;
       const progress = this.pastTime / this.duration;
       if (this.pastTime >= this.duration) {
-        this.emitter.emit('update', blender(this.easing, this.reverse)(1));
-        if (this.loop) {
+        this.emitter.emit('update', this.blend(1, this.turn));
+        if (this.loop || (this.round && !this.turn)) {
           this.startTime = timestamp;
+          if (this.round) {
+            this.turn = !this.turn;
+          }
         } else {
           this.pastTime = 0;
           this.rafId = 0;
+          this.turn = false;
           return;
         }
       } else {
-        this.emitter.emit('update', blender(this.easing, this.reverse)(progress));
+        this.emitter.emit('update', this.blend(progress, this.turn));
       }
       this.rafId = root.requestAnimationFrame(stepping);
     };
